ea9c5400a04c5dc47e494a9819fd0defe99e9673
JakubKosakowski <jkosakowski602@gmail.com>
2023-01-18 16:19:12 +0100

    Connect database to project

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask, render_template
+import psycopg2
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/hospital'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+conn = psycopg2.connect(database="hospital", user="postgres", password="password", host="localhost")
+mycursor = conn.cursor()
 
 
 @app.route('/')
