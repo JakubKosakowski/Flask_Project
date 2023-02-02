885c6cac4d3a534c5f78bdb81f2d4a7de32982cf
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 17:45:26 +0100

    Insert red tea type

--- database files/INSERT INTO type_of_product.sql
+++ database files/INSERT INTO type_of_product.sql
@@ -3,3 +3,5 @@ INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('green', '1-3 min', '70-80°C', '2-3g/120ml');
 
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('green japan', '1-3 min', '65-80°C', '2-3g/120ml');
+
+INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('red', '3-5 min', '95°C', '2-3g/120ml');
