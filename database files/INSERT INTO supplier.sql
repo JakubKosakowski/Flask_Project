b65ac666b4dbde8a567228885c9a96d188561457
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 14:56:08 +0100

    Insert Twinings company

--- database files/INSERT INTO supplier.sql
+++ database files/INSERT INTO supplier.sql
@@ -1,3 +1,5 @@
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, email) VALUES('Çaykur', 'İmdat Sütlüoğlu', 'Mufti Mah. Menderes Boulevard', '53080', 'RİZE', 'Turkey', 'caykur@caykur.gov.tr');
 
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Basilur Tea', 'Gamini Abeywickrama', '143/6, Weediya Bandara Mawatha, Kelanimulla Angoda', '10620', 'Ceylon', 'Sri Lanka', '+94112549500', 'info@basilurtea.com');
+
+INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Twinings', 'Bob Tavener', 'South Way', 'SP10 5AQ', 'Andover', 'UK', '+441264334477', 'twinings.direct@twinings.com');
