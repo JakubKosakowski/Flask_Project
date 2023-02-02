e76e4034a946634bafc06bfa014e2299f4695bea
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 14:55:13 +0100

    Insert Basilur Tea company

--- database files/INSERT INTO supplier.sql
+++ database files/INSERT INTO supplier.sql
@@ -1,1 +1,3 @@
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, email) VALUES('Çaykur', 'İmdat Sütlüoğlu', 'Mufti Mah. Menderes Boulevard', '53080', 'RİZE', 'Turkey', 'caykur@caykur.gov.tr');
+
+INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Basilur Tea', 'Gamini Abeywickrama', '143/6, Weediya Bandara Mawatha, Kelanimulla Angoda', '10620', 'Ceylon', 'Sri Lanka', '+94112549500', 'info@basilurtea.com');
