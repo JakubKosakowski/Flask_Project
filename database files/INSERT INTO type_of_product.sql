8533913439576bdd6e4c2f2d2454525368eee21a
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 17:55:50 +0100

    Insert Vietnam oolong tea type

--- database files/INSERT INTO type_of_product.sql
+++ database files/INSERT INTO type_of_product.sql
@@ -11,3 +11,5 @@ INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('yellow', '2-3 min', '80-85°C', '2-3g/120ml');
 
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('oolong', '1-3 min', '90°C', '3g/120ml');
+
+INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('oolong vietnam', '1-3 min', '75-85°C', '3g/120ml');
