91d934ca9908809f5edc8883673d80dae824e76c
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 17:44:09 +0100

    Insert Japan green tea type

--- database files/INSERT INTO type_of_product.sql
+++ database files/INSERT INTO type_of_product.sql
@@ -1,3 +1,5 @@
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('black', '1-3 min', '95°C', '3g/120ml');
 
 INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('green', '1-3 min', '70-80°C', '2-3g/120ml');
+
+INSERT INTO type_of_product(name, brewing_time, brewing_temperature, amount_per_cup) VALUES('green japan', '1-3 min', '65-80°C', '2-3g/120ml');
