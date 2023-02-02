8357aa4bb1136fe044a0a5652c68b3fbfb3a3d94
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 16:51:34 +0100

    Insert Intelligensia Coffee company

--- database files/INSERT INTO supplier.sql
+++ database files/INSERT INTO supplier.sql
@@ -11,3 +11,5 @@ INSERT INTO supplier(company_name, contact_name, address, postal_code, city, cou
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Yogi Tea', ' Giancarlo Marcaccin', 'Burchardstraße 24', '20095', 'Hamburg', 'Germany', '+4904042301119', ' info.eu@yogiproducts.com');
 
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Lavazza', 'Antonio Baravalle', 'Via Bologna 32', '10152', 'Turin', 'Italy', '+3901123981', 'info@lavazza.it');
+
+INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Intelligentsia Coffee', 'James McLaughlin', '1850 W. Fulton Street', '60612', 'Chicago', 'USA', '3122530594', 'customersupport@intelligentsiacoffee.com')
