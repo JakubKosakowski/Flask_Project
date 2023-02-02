fe68c23d21efd53630e023d52c23fea0cdd2cd61
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-02 14:57:53 +0100

    Insert Unilever company

--- database files/INSERT INTO supplier.sql
+++ database files/INSERT INTO supplier.sql
@@ -3,3 +3,5 @@ INSERT INTO supplier(company_name, contact_name, address, postal_code, city, cou
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Basilur Tea', 'Gamini Abeywickrama', '143/6, Weediya Bandara Mawatha, Kelanimulla Angoda', '10620', 'Ceylon', 'Sri Lanka', '+94112549500', 'info@basilurtea.com');
 
 INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Twinings', 'Bob Tavener', 'South Way', 'SP10 5AQ', 'Andover', 'UK', '+441264334477', 'twinings.direct@twinings.com');
+
+INSERT INTO supplier(company_name, contact_name, address, postal_code, city, country, phone, email) VALUES('Unilever', 'Hein Schumacher', '100 Victoria Embankment', 'EC4Y 0DY', 'London', 'UK', '+441372945000', 'webqueries@computershare.co.uk');
