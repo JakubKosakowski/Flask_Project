d8ece9832824bf44489acdb79c1ec8c3b033a2ed
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-05 18:52:43 +0100

    Connect store db and show suppliers

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
 from flask import Flask, render_template
-import psycopg2
+from sql_utils import execute_sql
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/northwind'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-conn = psycopg2.connect(database="northwind", user="postgres", password="password", host="localhost")
-mycursor = conn.cursor()
-
-
 @app.route('/')
 def home():
-    mycursor.execute("SELECT * FROM orders")
-    data = mycursor.fetchall()
+    data = execute_sql(
+        "datab",
+        """
+            SELECT company_name, address, email
+            FROM supplier;
+        """
+    )
     return render_template('home.html', data=data)
 
 
