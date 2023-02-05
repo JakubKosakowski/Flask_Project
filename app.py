e177763b7a3218fbfb38572a76b0bea758373d33
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-05 18:44:53 +0100

    Add app.run() to project

--- app.py
+++ app.py
@@ -3,15 +3,15 @@ import psycopg2
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/hospital'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:password@localhost/northwind'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-conn = psycopg2.connect(database="hospital", user="postgres", password="password", host="localhost")
+conn = psycopg2.connect(database="northwind", user="postgres", password="password", host="localhost")
 mycursor = conn.cursor()
 
 
 @app.route('/')
 def home():
-    mycursor.execute("SELECT * FROM doctors")
+    mycursor.execute("SELECT * FROM orders")
     data = mycursor.fetchall()
     return render_template('home.html', data=data)
 
@@ -19,3 +19,7 @@ def home():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
