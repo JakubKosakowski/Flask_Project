33da21ba262145dd6c9e1705ff9f5ac078c29407
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-06 17:06:54 +0100

    Add Twinings English Breakfast tea image

--- database files/ADD COLUMN & UPDATE image.sql
+++ database files/ADD COLUMN & UPDATE image.sql
@@ -1,5 +1,7 @@
 ALTER TABLE product ADD COLUMN image VARCHAR(200) DEFAULT NULL;
 
+UPDATE product SET image = 'twinings_english_breakfast_image.png' WHERE id = 4;
+
 UPDATE product SET image = 'twinings_earl_grey_image.png' WHERE id = 5;
 
 UPDATE product SET image = 'twinings_lady_grey_image.png' WHERE id = 6;
