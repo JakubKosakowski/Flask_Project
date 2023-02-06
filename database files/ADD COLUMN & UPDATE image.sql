7e82331f46bda2de417b631ae28d7013b0fff23b
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-06 17:02:44 +0100

    Add Twinings Lady Grey tea image

--- database files/ADD COLUMN & UPDATE image.sql
+++ database files/ADD COLUMN & UPDATE image.sql
@@ -1,3 +1,5 @@
 ALTER TABLE product ADD COLUMN image VARCHAR(200) DEFAULT NULL;
 
 UPDATE product SET image = 'twinings_earl_grey_image.png' WHERE id = 5;
+
+UPDATE product SET image = 'twinings_lady_grey_image.png' WHERE id = 6;
