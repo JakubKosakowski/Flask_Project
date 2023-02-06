0756ef98bdf61c9937f2812d0e4218e9573141e8
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-06 19:24:09 +0100

    Add Caykur Earl Grey tea image

--- database files/ADD COLUMN & UPDATE image.sql
+++ database files/ADD COLUMN & UPDATE image.sql
@@ -1,5 +1,7 @@
 ALTER TABLE product ADD COLUMN image VARCHAR(200) DEFAULT NULL;
 
+UPDATE product SET image = 'caykur_earl_grey_image.png' WHERE id = 2;
+
 UPDATE product SET image = 'twinings_english_breakfast_image.png' WHERE id = 4;
 
 UPDATE product SET image = 'twinings_earl_grey_image.png' WHERE id = 5;
