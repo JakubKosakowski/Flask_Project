207add5e2c5c35bbc68fd81f89fcd05575be89ff
JakubKosakowski <jkosakowski602@gmail.com>
2023-02-06 19:33:04 +0100

    Add Caykur Rize Turist tea image

--- database files/ADD COLUMN & UPDATE image.sql
+++ database files/ADD COLUMN & UPDATE image.sql
@@ -1,5 +1,7 @@
 ALTER TABLE product ADD COLUMN image VARCHAR(200) DEFAULT NULL;
 
+UPDATE product SET image = 'caykur_rize_turist_tea_image.png' WHERE id = 1;
+
 UPDATE product SET image = 'caykur_earl_grey_image.png' WHERE id = 2;
 
 UPDATE product SET image = 'caykur_filiz_tea_image.png' WHERE id = 3;
